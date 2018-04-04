4b4629cbd9b72e1717c48eb3a7ff6db795a9eb19
jeffliang0318 <liangnash0318@gmail.com>
2018-04-03 17:26:02 -0700

    can make GET request and get user info

--- db/migrate/20180403223044_create_users.rb
+++ db/migrate/20180403223044_create_users.rb
@@ -15,12 +15,12 @@ end
 
 # def change
 #     create_table :users do |t|
-#       t.string :username, null: false
+#       t.string :, null: false
 #       t.string :password_digest, null: false
 #       t.string :session_token, null: false
 #
 #       t.timestamps
 #     end
-#     add_index :users, :username, unique: true
+#     add_index :users, :, unique: true
 #     add_index :users, :session_token, unique: true
 #   end
