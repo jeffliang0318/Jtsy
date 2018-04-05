b88927153423b214005bd99333109efbc51e1de0
jeffliang0318 <liangnash0318@gmail.com>
2018-04-04 18:34:58 -0700

    add app back

---
+++ frontend/components/app.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import Modal from './modal/modal';
+import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_form/login_form_container.jsx';
+import SignupFormContainer from './session_form/signup_form_container.jsx';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <Modal />
+    <header>
+      <Link to="/" className="header-link">
+        <h1>Jtsy</h1>
+      </Link>
+      <GreetingContainer />
+    </header>
+  </div>
+);
+
+export default App;
