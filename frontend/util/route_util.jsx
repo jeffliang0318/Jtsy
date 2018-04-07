6879bc843e40451198c7a5238975cdca706d1169
jeffliang0318 <liangnash0318@gmail.com>
2018-04-07 13:01:43 -0700

    add open modal to mapdispatch

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
-
+import { openModal } from '../actions/modal_actions';
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
     !loggedIn ? (
@@ -24,6 +24,10 @@ const mapStateToProps = state => (
   {loggedIn: Boolean(state.ui.session.currentUser)}
 );
 
+const mapDispatchToProps = dispatch => ({
+  openModal: modal => dispatch(openModal(modal))
+});
+
 export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
 
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, mapDispatchToProps)(Protected));
